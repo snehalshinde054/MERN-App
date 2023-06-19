1be1bf92ea89c65dbefab0b8ae64d3136aa6bb06
Trisha <snehalshinde054@gmail.com>
2023-06-19 18:40:33 +0530

    Added code for update and delete API[COMPLETE]

--- backend/routes.js
+++ backend/routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const Employee = require('./employee.js');
 
+const ObjectId = require('mongoose').Types.ObjectId;
+
 // GET API
 router.get('/',async (req,res) =>{
     
@@ -31,5 +33,74 @@ router.post('/',async (req,res)=>{
             res.json({message:err });
         }
 });
+
+// GET paricular employee data API
+router.get('/:id',(req,res)=>{
+
+    (async () => {
+        try {
+            if(ObjectId.isValid(req.params.id)){
+                const result = await Employee.findById(req.params.id);
+                res.json(result);
+            }
+            else
+            {
+                return res.status(400).send(`No record found with ID ${req.params.id}`);
+            }
+        } catch (err) {
+            res.json({message:err });
+        }
+      })();
+
+})
+// Search employee API
+
+// Update employee data API
+router.put('/:id',(req,res)=>{
+
+    (async () => {
+        try {
+           
+            if(ObjectId.isValid(req.params.id)){
+                let emp={
+                    name: req.body.name,
+                    position:req.body.position,
+                    dept:req.body.dept
+                };
+
+                const result = await Employee.findByIdAndUpdate(req.params.id,emp);
+                res.json(emp);
+            }
+            else
+            {
+                return res.status(400).send(`No record found with ID ${req.params.id}`);
+            }
+        } catch (err) {
+            res.json({message:err });
+        }
+      })();
+
+})
+
+
+// Delete employee API
+router.delete('/:id',(req,res)=>{
+
+    (async () => {
+        try {
+            if(ObjectId.isValid(req.params.id)){
+                const result = await Employee.findByIdAndDelete(req.params.id);
+                res.json(result);
+            }
+            else
+            {
+                return res.status(400).send(`No record found with ID ${req.params.id}`);
+            }
+        } catch (err) {
+            res.json({message:err });
+        }
+      })();
+
+})
 
 module.exports= router;
